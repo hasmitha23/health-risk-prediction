1d66ae398b23989374557e17240c35900af1bb1f
Nalamati Hasmitha <hasmithanalamati@gmail.com>
2026-02-17 20:48:09 +0530

    Update app.py

--- health_risk_app/app.py
+++ health_risk_app/app.py
@@ -472,7 +472,10 @@ def view_report(report_id):
 
 
 if __name__ == "__main__":
-        app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 
 
